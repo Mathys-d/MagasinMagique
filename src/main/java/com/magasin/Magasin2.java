e81a5c6704db9561e66d98701dd2ae3b50646f63
mathys-d <mathys.deronne@le-campus-numerique.fr>
2025-09-04 16:43:15 +0200

    updateQuality2 changes

--- src/main/java/com/magasin/Magasin2.java
+++ src/main/java/com/magasin/Magasin2.java
@@ -12,19 +12,18 @@ class Magasin2 {
     public void updateQuality2() {
         for (int i = 0; i < items.length; i++) {
 
-
             if (items[i].quality >= 50 &&  !Objects.equals(items[i].name, "Kryptonite")) {
                 items[i].quality = 50;
             }
             if  (items[i].sellIn <= 0 && !Objects.equals(items[i].name, "Kryptonite")) {
-                items[i].quality -= 1;
-            } items[i].quality -= 1 ;
-            items[i].sellIn -= 1 ;
+                items[i].quality = items[i].quality- 1;
+            } items[i].quality = items[i].quality- 1 ;
+            items[i].sellIn = items[i].sellIn - 1 ;
 
 
             if (Objects.equals(items[i].name, "Kryptonite")){
                 items[i].quality = 80;
-                items[i].sellIn += 1;
+                items[i].sellIn = items[i].sellIn + 1;
             }
 
             if (Objects.equals(items[i].name, "Pass VIP Concert")) {
@@ -40,7 +39,7 @@ class Magasin2 {
             }
 
             if (Objects.equals(items[i].name, "Comté")) {
-                items[i].quality += 2;
+                items[i].quality = items[i].quality + 2;
             }
             if (items[i].quality <= 0) {
                 items[i].quality = 0;
